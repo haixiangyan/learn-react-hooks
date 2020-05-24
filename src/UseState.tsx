23f7ee490c0efc41c51d73e9264b7486d49edf6e
Haixiang <haixiang6123@gmail.com>
2020-05-25 07:24:18 +0800

    添加 obj

--- src/UseState.tsx
+++ src/UseState.tsx
@@ -4,9 +4,14 @@ import {useState} from 'react'
 const UseState: React.FC = () => {
   const [x, setX] = useState(0)
   const [y, setY] = useState(100)
+  const [obj, setObj] = useState({name: 'Jack', age: 18})
 
   const onClickX = () => setX(x + 1)
   const onClickY = () => setY((prevX) => prevX + 1)
+  const onClickObj = () => setObj({
+    ...obj,
+    age: obj.age + 1
+  })
 
   return (
     <div>
@@ -16,6 +21,9 @@ const UseState: React.FC = () => {
 
       <button onClick={onClickY}>y + 1</button>
       <p>y: {y}</p>
+
+      <button onClick={onClickObj}>age + 1</button>
+      <p>{JSON.stringify(obj)}</p>
     </div>
   )
 }
